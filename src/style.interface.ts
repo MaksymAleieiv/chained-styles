b1d96a4fb4c82f17265988cbf28acd4dab0510ba
MaksymAleieiv <maxaleieiv@gmail.com>
2025-06-03 22:55:17 +0300

    a commit that fixes a mess created by inital commit

--- src/style.interface.ts
+++ src/style.interface.ts
@@ -1,11 +1,13 @@
-import { Theme } from 'chained-styles';
-
 export type ChainedStylesColorsType = Record<string | number, string>;
 
 export interface ChainedStylesInterface {
   getSize: (size: number) => number;
 }
 
+export interface Theme {}
+
+export interface ThemeInterface extends Theme, ChainedStylesInterface {}
+
 export type ChainedStylesThemeType<T extends Record<string, any> = object> = T & {
-  theme: Theme;
+  theme: ThemeInterface
 };
